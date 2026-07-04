637221f54881a045d4ebd96e4f611d6482163c26
Henrique Caldas <henrique.c.caldas@tum.de>
2026-07-04 10:23:20 +0200

    Adding core module for logistic map

--- include/classicMaps.h
+++ include/classicMaps.h
@@ -1,20 +1,46 @@
 #include "map.h"
+#include <stdexcept>
 
 class DoublingMap : public Map {
 public:
-    double iterate(double x)    const override { \
+    double iterate(double x) const override {
         return std::fmod(2.0 * x, 1.0); 
     }
     double derivative(double /*x*/) const override { 
         return 2.0; 
     }
-    std::string name()          const override { 
+    std::string name() const override { 
         return "DoublingMap";
     }
-    double xmin()               const override { 
+    double xmin() const override { 
         return 0.0; 
     }
-    double xmax()               const override { 
+    double xmax()  const override { 
+        return 1.0; 
+    }
+};
+
+// Logistic map: T(x) = r * x * (1 - x), x in [0,1]
+
+class LogisticMap : public Map {
+private:
+    //Parameter for the logistic regression
+    double r_;
+public:
+    explicit LogisticMap(double r);
+    double iterate(double x) const override{
+        return (r_ * x * (1-x));
+    }
+    double derivative (double x) const override {
+        return (r_ * (1 - 2 * x));
+    }
+    std::string name() const override {
+        return ("Logistic Map at " + std::to_string(r_));
+    }
+    double xmin() const override {
+        return 0.0;
+    }
+    double xmax() const override {
         return 1.0; 
     }
 };
--- src/classicMaps.cpp
+++ src/classicMaps.cpp
@@ -1,4 +1,11 @@
 #include "../include/classicMaps.h"
 
 
+//Logistic Map
+LogisticMap::LogisticMap(double r){
+    if (r > 4 || r < 0){
+        throw std::invalid_argument("parameter r must be between 0 and 4.0");
+    }
+    else r_ = r;
+}
 
