acfd994ae78170442e730ac4ae63995b74205077
Henrique Colaço e Caldas <henriquecolacoecaldas@gmail.com>
2026-08-19 22:45:45 +0200

    Move point and matrix to public member

--- include/mapTemplate.h
+++ include/mapTemplate.h
@@ -3,8 +3,9 @@
 #include <boost/multiprecision/cpp_bin_float.hpp>
 #include <boost/multiprecision/fwd.hpp>
 #include <vector>
+#include <array>
 #include <string>
- 
+
 
 //Extremely important. Real numbers should be used as real("0.2") and not real r = 0.2. Otherwise we have the same issue as with a double
 
@@ -15,11 +16,12 @@ using real = boost::multiprecision::cpp_bin_float_100;
 // using const methods to guarantee no member of the class is changed when called
 
 template <size_t dim> class Map{
-    
-    using point = std::array<real, dim>;
-    using matrix = std::array<std::array<real , dim>>;
 
     public:
+    
+        using point = std::array<real, dim>;
+        using matrix = std::array<std::array<real, dim>, dim>;
+
         //Phase space bounds [xmin, xmax]
         virtual point xmin() const = 0;
         virtual point xmax() const = 0;
@@ -28,7 +30,7 @@ template <size_t dim> class Map{
         virtual ~Map() = default;
 
         // Apply the map once: x -> T(x)
-        virtual point iterate(real x) const = 0;
+        virtual point iterate(point x) const = 0;
 
         // Jacobian of the map T at x: DT(x)
         virtual matrix jacobian(real x) const = 0;
@@ -44,8 +46,8 @@ template <size_t dim> class Map{
         std::vector<point> orbit(point x0, int n) const;
 
     protected:
-
-        //To change any params in the derived class, use this constructor 
+    
+        //To change any params in the derived class, use this constructor
         //to have access to the params_ methods from the base class
         Map(std::string tag, std::vector<real> params): tag_(std::move(tag)), params_(std::move(params)) {}
         //The derived classes must have an alias parameter as a member for these parameters stores in the params vector
