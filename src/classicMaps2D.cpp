f94e8fd7fea2419da3bb59bb3ed38a0eba010d16
Henrique Colaço e Caldas <henriquecolacoecaldas@gmail.com>
2026-08-21 22:59:50 +0200

    Add iterate() and jacobian() of the Rulkov Map

--- src/classicMaps2D.cpp
+++ src/classicMaps2D.cpp
@@ -1,13 +1,28 @@
 #include "../include/classicMaps2D.h"
+#include "../include/mapTemplate.h"
 
-HenonMap::point HenonMap::iterate(point x) const {
+Map2D::point HenonMap::iterate(point x) const {
     return {real("1") - alpha_ * x[0] * x[0] + x[1], beta_ * x[0]};
 }
 
-HenonMap::matrix HenonMap::jacobian(point x) const {
+Map2D::matrix HenonMap::jacobian(point x) const {
     return {{
         {real("-2") * alpha_ * x[0], real("1")},
         {beta_, real("0")}
+    }};
+}
+
+Map2D::point RulkovMap::iterate(point x) const {
+    return{
+        (alpha_ / (real("1") + (x[0]*x[0]))) + x[1],
+        x[1] - mu_ * (x[0] - sigma_)
+    };
+}
+
+Map2D::matrix RulkovMap::jacobian(point x) const{
+    return {{
+        {(-real("2")*alpha_*(x[0]/(real("1") + (x[0]*x[0])))), 1},
+        {-mu_, 1}
     }};
 }
 
