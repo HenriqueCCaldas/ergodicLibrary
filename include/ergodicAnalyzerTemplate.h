7ab767d0ac11a449d8bf4d1fc12021e12a1446db
Henrique Colaço e Caldas <henriquecolacoecaldas@gmail.com>
2026-08-24 23:04:34 +0100

    Fix definition in header and cpp with typename to make proper compiling with template <size_t dim>

--- include/ergodicAnalyzerTemplate.h
+++ include/ergodicAnalyzerTemplate.h
@@ -5,19 +5,21 @@
 #include <numeric>
 #include <stdexcept>
 
-template <size_t dim> class Analyzer {
+template <size_t dim> class Analyzer{
     private:
         const Map<dim>& map_;
         
     public:
+
         explicit Analyzer(const Map<dim>& map): map_(map){};
         
-        Map<dim>::point birkhoffAverage(point x0, int N, const std::function<point(point)>& f) const;
+        typename Map<dim>::point birkhoffAverage(typename Map<dim>::point x0, int N,
+            const std::function< typename Map<dim>::point(typename Map<dim>::point)>& f) const;
 
         // Birkhoff average convergence: returns vector of running averages
-        std::vector<Map<dim>::point> birkhoffConvergence(point x0, int N, const std::function<point(point)>& f) const;
+        std::vector<typename Map<dim>::point> birkhoffConvergence(point x0, int N, const std::function<point(point)>& f) const;
 
         //Trajectory divergence: log|x_n - y_n| for two orbits starting delta apart (x0, x0+delta).
-        std::vector<Map<dim>::point> trajectoryDivergence(point x0, int N, point delta) const;
+        std::vector<typename Map<dim>::point> trajectoryDivergence(point x0, int N, point delta) const;
 
 };
--- src/ergodicAnalyzerTemplate.cpp
+++ src/ergodicAnalyzerTemplate.cpp
@@ -1,2 +1,16 @@
 #include "../include/ergodicAnalyzerTemplate.h"
 
+
+template <size_t dim> typename Map<dim>::point Analyzer<dim>::birkhoffAverage(typename Map<dim>::point x0, int N,
+    const std::function< typename Map<dim>::point(typename Map<dim>::point)>& f) const{
+
+}
+// Birkhoff average convergence: returns vector of running averages
+template <size_t dim>
+std::vector<typename Map<dim>::point> birkhoffConvergence(typename Map<dim>::point x0, int N, 
+    const std::function<typename Map<dim>::point(typename Map<dim>::point)>& f) const;
+
+//Trajectory divergence: log|x_n - y_n| for two orbits starting delta apart (x0, x0+delta).
+template <size_t dim>
+std::vector<typename Map<dim>::point> trajectoryDivergence(typename Map<dim>::point x0, int N,
+    typename Map<dim> point delta) const;
