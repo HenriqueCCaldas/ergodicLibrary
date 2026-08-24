9fccc2e00c333fc4b12edef8e3d626a8477700d4
Henrique Colaço e Caldas <henriquecolacoecaldas@gmail.com>
2026-08-25 00:52:07 +0100

    Add logic to the Birkhoff Average and Convergence

--- include/ergodicAnalyzerTemplate.h
+++ include/ergodicAnalyzerTemplate.h
@@ -13,13 +13,14 @@ template <size_t dim> class Analyzer{
 
         explicit Analyzer(const Map<dim>& map): map_(map){};
         
-        typename Map<dim>::point birkhoffAverage(typename Map<dim>::point x0, int N,
-            const std::function< typename Map<dim>::point(typename Map<dim>::point)>& f) const;
+        real birkhoffAverage(typename Map<dim>::point x0, int N,
+            const std::function< real(typename Map<dim>::point)>& f) const;
 
         // Birkhoff average convergence: returns vector of running averages
-        std::vector<typename Map<dim>::point> birkhoffConvergence(point x0, int N, const std::function<point(point)>& f) const;
+        std::vector<real> birkhoffConvergence(typename Map<dim>::point x0, int N, const std::function<real(typename Map<dim>::point)>& f) const;
 
         //Trajectory divergence: log|x_n - y_n| for two orbits starting delta apart (x0, x0+delta).
         std::vector<typename Map<dim>::point> trajectoryDivergence(point x0, int N, point delta) const;
 
 };
+
--- src/ergodicAnalyzerTemplate.cpp
+++ src/ergodicAnalyzerTemplate.cpp
@@ -1,14 +1,35 @@
 #include "../include/ergodicAnalyzerTemplate.h"
+#include "../include/mapTemplate.h"
 
 
-template <size_t dim> typename Map<dim>::point Analyzer<dim>::birkhoffAverage(typename Map<dim>::point x0, int N,
-    const std::function< typename Map<dim>::point(typename Map<dim>::point)>& f) const{
-
+template <size_t dim>
+real Analyzer<dim>::birkhoffAverage(typename Map<dim>::point x0, int N,
+    const std::function< real(typename Map<dim>::point)>& f) const{
+//Copy what is already defined for 1D in ergodicAnalyzer.cpp
+        real sum = real("0.0");
+        real x = x0;
+        // Birkhoff average: (1/N) * sum_{n=0}^{N-1} f(T^n x0)
+        for (int i = 0; i < N; i++) {
+            sum += f(x);
+            x = map_.iterate(x);
+        }
+        return sum / N;
 }
 // Birkhoff average convergence: returns vector of running averages
 template <size_t dim>
-std::vector<typename Map<dim>::point> birkhoffConvergence(typename Map<dim>::point x0, int N, 
-    const std::function<typename Map<dim>::point(typename Map<dim>::point)>& f) const;
+std::vector<real> Analyzer<dim>::birkhoffConvergence(typename Map<dim>::point x0, int N, const std::function<real(typename Map<dim>::point)>& f) const{
+        std::vector<real> running;
+        running.reserve(N);
+        real sum = real("0.0");
+        real x = x0;
+        // Birkhoff average: (1/N) * sum_{n=0}^{N-1} f(T^n x0)
+        for (int i = 0; i < N; i++) {
+            sum += f(x);
+            running.append(x);
+            x = map_.iterate(x);
+        }
+        return sum / N;
+}
 
 //Trajectory divergence: log|x_n - y_n| for two orbits starting delta apart (x0, x0+delta).
 template <size_t dim>
